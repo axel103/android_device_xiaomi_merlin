02151c4bea4f2d4002984fc80b6a1b1099bb44d1
axel103 <131704687+axel103@users.noreply.github.com>
2024-04-22 18:21:09 +0200

    Add files via upload

---
+++ omni_merlin.mk
@@ -0,0 +1,34 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Add this line if your device is 64-bit
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+# Otherwise, If you have 32-bit device, add the below line instead of above line
+# $(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk
+
+# Another common config inclusion
+$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
+
+# If you are building from OmniROM's minimal source, Inherit some common Omni stuff.
+# $(call inherit-product, vendor/omni/config/common.mk)
+
+# Replace ${_CODENAME_} with your Device Name's Value.
+# Replace ${_VENDORNAME_} with your Brand's / Manufacturer's Value.
+PRODUCT_COPY_FILES += device/xiaomi/merlin/prebuilt/kernel:kernel
+# Fles under $(LOCAL_PATH)/recovery/root/ gets automatically copied into recovery
+# PRODUCT_COPY_FILES += $(LOCAL_PATH)/recovery/root/*:root/*
+
+PRODUCT_DEVICE := merlin
+PRODUCT_NAME := omni_merlin
+PRODUCT_BRAND := xiaomi
+PRODUCT_MODEL := merlin
+PRODUCT_MANUFACTURER := Xiaomi
+
+# Forcefully add mtp support (adb is already there)
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp
+
+# Add fingerprint from Stock ROM build.prop
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    # These lines are from my device. You MUST Replace yours.
+    BUILD_FINGERPRINT="Redmi/merlin_global/merlin:12/SP1A.210812.016/V13.0.2.0.SJOMIXM:user/release-keys" \
+    PRIVATE_BUILD_DESC="merlin-user 12 SP1A.210812.016 V13.0.2.0.SJOMIXM release-keys""
